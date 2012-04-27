7a816616e54e35e2fadf85799b736fd08856266e
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-04-27 22:58:01 +0100

    Update CHANGES and bump to version 0.36

--- lib/Params/Check.pm
+++ lib/Params/Check.pm
@@ -16,7 +16,7 @@ BEGIN {
     @ISA        =   qw[ Exporter ];
     @EXPORT_OK  =   qw[check allow last_error];
 
-    $VERSION                = '0.34';
+    $VERSION                = '0.36';
     $VERBOSE                = $^W ? 1 : 0;
     $NO_DUPLICATES          = 0;
     $STRIP_LEADING_DASHES   = 0;
